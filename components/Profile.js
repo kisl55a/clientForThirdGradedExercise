d1c94b2efa21fa88266c182e9d3f7bb0fd0f2774
Dmitrii <kislenko1910@mail.ru>
2020-02-29 11:50:15 +0200

    Profile fetchs and shows the data

--- components/Profile.js
+++ components/Profile.js
@@ -1,38 +1,68 @@
 import React, { useEffect, useState } from 'react'
 import { View, Text, StyleSheet, Button } from 'react-native'
 import { useSelector, useDispatch } from 'react-redux'
+import { ScrollView } from 'react-native-gesture-handler';
+import Constants from 'expo-constants';
+import ProfileItemSmall from './ProfileItemSmall'
 import allActions from '../src/actions/index'
 const requestFunctions = require('./functions/requestsFunctions');
 
 const Profile = () => {
     const currentUser = useSelector(state => state.currentUser)
     const dispatch = useDispatch();
+    const [itemsData, setItemsData] = useState([])
     useEffect(() => {
         requestFunctions.authorizedGetRequest('https://graded-exercise-kidm.herokuapp.com/items/', currentUser.token)
             .then(data => {
-                console.log(data)
+                setItemsData(data)
             })
             .catch(err => console.log(err))
     }, [])
     return (
-        <View style={styles.container}>
-            <Text>Hello, {currentUser.username}</Text>
-            <Button onPress={() => 
-            dispatch(allActions.userActions.setUser({
-                username: '',
-                token: ''
-            }))}
-            title = 'Logout'
-            />
-        </View>
+        <ScrollView style={{ backgroundColor: "#fff" }}>
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <Text style={{fontSize: 30, marginBottom: 10}}>Hello, {currentUser.username}</Text>
+                    <View style={{flexDirection: "row"}}>
+                        <Button onPress={() =>
+                            dispatch(allActions.userActions.setUser({
+                                username: '',
+                                token: ''
+                            }))}
+                            title='Add new item'
+                        />
+                        <Button onPress={() =>
+                            dispatch(allActions.userActions.setUser({
+                                username: '',
+                                token: ''
+                            }))}
+                            title='Logout'
+                        />
+                    </View>
+                </View>
+                {itemsData.map(element => {
+                    return (
+                        <ProfileItemSmall key={element.id} data={element}></ProfileItemSmall>
+                      )
+                })}
+            </View>
+        </ScrollView>
+
     )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+        marginTop: Constants.statusBarHeight,
+        backgroundColor: '#fff',
+        alignItems: "center"
+    },
+    header: {
+        width: "100%",
+        paddingHorizontal: 10,
+        justifyContent: "center",
+        alignItems: "center"
     }
 })
 
