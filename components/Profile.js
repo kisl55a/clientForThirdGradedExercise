369472c57fc6c66fca1862733ca5cd07319a64ab
Dmitrii <kislenko1910@mail.ru>
2020-02-29 11:29:33 +0200

    Fixed the problem with hooks

--- components/Profile.js
+++ components/Profile.js
@@ -1,10 +1,29 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { View, Text, StyleSheet, Button } from 'react-native'
+import { useSelector, useDispatch } from 'react-redux'
+import allActions from '../src/actions/index'
+const requestFunctions = require('./functions/requestsFunctions');
 
 const Profile = () => {
+    const currentUser = useSelector(state => state.currentUser)
+    const dispatch = useDispatch();
+    useEffect(() => {
+        requestFunctions.authorizedGetRequest('https://graded-exercise-kidm.herokuapp.com/items/', currentUser.token)
+            .then(data => {
+                console.log(data)
+            })
+            .catch(err => console.log(err))
+    }, [])
     return (
         <View style={styles.container}>
-            <Text>Profile</Text>
+            <Text>Hello, {currentUser.username}</Text>
+            <Button onPress={() => 
+            dispatch(allActions.userActions.setUser({
+                username: '',
+                token: ''
+            }))}
+            title = 'Logout'
+            />
         </View>
     )
 }
--- components/functions/requestsFunctions.js
+++ components/functions/requestsFunctions.js
@@ -35,5 +35,23 @@ module.exports = {
             // body: JSON.stringify(data) // body data type must match "Content-Type" header
         });
         return await response.json(); // parses JSON response into native JavaScript objects
+    },
+    authorizedGetRequest: async (url = '', token) => {
+        // Default options are marked with *
+        const response = await fetch(url, {
+            method: 'GET', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+                'Content-Type': 'application/json',
+                "Authorization": `Bearer ${token}`
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer', // no-referrer, *client
+            // body: JSON.stringify(data) // body data type must match "Content-Type" header
+        });
+        return await response.json(); // parses JSON response into native JavaScript objects
     }
 }
