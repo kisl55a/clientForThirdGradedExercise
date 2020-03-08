472557fe176c44d357feee815ae10c803a13eef8
Dmitrii <kislenko1910@mail.ru>
2020-03-08 20:18:05 +0200

    Profile styling is done

--- components/Profile.js
+++ components/Profile.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
-import { View, Text, StyleSheet, Button } from 'react-native'
+import { View, Text, StyleSheet, Button, TouchableOpacity, Image } from 'react-native'
 import { useSelector, useDispatch } from 'react-redux'
 import { ScrollView } from 'react-native-gesture-handler';
 import Constants from 'expo-constants';
 import ProfileItemSmall from './ProfileItemSmall'
 import allActions from '../src/actions/index'
 const requestFunctions = require('./functions/requestsFunctions');
+
 
 
 const Profile = (props) => {
@@ -22,27 +23,51 @@ const Profile = (props) => {
     }, [trigger])
     return (
         <ScrollView style={{ backgroundColor: "#fff" }}>
+
             <View style={styles.container}>
-                <View style={styles.header}>
-                    <Text style={{fontSize: 30, marginBottom: 10}}>Hello, {currentUser.username}</Text>
-                    <View style={{flexDirection: "row"}}>
-                        <Button onPress={() => props.navigation.navigate('CreateNewItem')
-                            }
-                            title='Add new item'
-                        />
-                        <Button onPress={() =>
-                            dispatch(allActions.userActions.setUser({
-                                username: '',
-                                token: ''
-                            }))}
-                            title='Logout'
-                        />
+
+                <View style={{ flexDirection: "row", justifyContent: "space-between", width: "90%", alignItems: "center", marginLeft: "5%" }}>
+
+                    <View>
+                        <Text style={{ fontSize: 40, fontWeight: "800" }}>
+                            {currentUser.username}
+                        </Text>
+                        <TouchableOpacity onPress={() => props.navigation.navigate('CreateNewItem')}>
+                            <Text style={{ fontSize: 20, color: "#12A1D7" }}>Add new item</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => dispatch(allActions.userActions.setUser({
+                            username: '',
+                            token: ''
+                        }))}>
+                            <Text style={{ fontSize: 20, color: "#12A1D7" }}>Logout</Text>
+                        </TouchableOpacity>
+
                     </View>
+                    <Image
+                        source={{ uri: "https://cdn0.iconfinder.com/data/icons/social-media-network-4/48/male_avatar-512.png", width: 100, height: 100 }}
+                    />
                 </View>
+                <View
+                    style={{
+                        paddingVertical: 10,
+                        borderBottomColor: 'lightgray',
+                        borderBottomWidth: 0.5,
+                    }}
+                />
+                {(itemsData.length == 0 ? 
+                 <Text style={{ fontSize: 20, fontWeight: "800", marginLeft: "3%", marginTop: 10 }}>
+                 You have nothing on sell now 
+                     </Text>
+                     :
+                     <Text style={{ fontSize: 20, fontWeight: "800", marginLeft: "3%", marginTop: 10 }}>
+                     On sell now:
+                         </Text>
+                )}
+             
                 {itemsData.map(element => {
                     return (
                         <ProfileItemSmall {...props} key={element.id} data={element}></ProfileItemSmall>
-                      )
+                    )
                 })}
             </View>
         </ScrollView>
@@ -55,14 +80,9 @@ const styles = StyleSheet.create({
         flex: 1,
         marginTop: Constants.statusBarHeight,
         backgroundColor: '#fff',
-        alignItems: "center"
+        // alignItems: "center"
     },
-    header: {
-        width: "100%",
-        paddingHorizontal: 10,
-        justifyContent: "center",
-        alignItems: "center"
-    }
+
 })
 
 export default Profile
